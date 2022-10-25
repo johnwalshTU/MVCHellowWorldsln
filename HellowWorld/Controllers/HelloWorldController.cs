5837efda9f300b6d2f49547e2a5141f340d3aab4
John Walsh <johnwalsh@gmail.com>
2022-10-25 17:20:03 +0100

    Add project files.

---
+++ HellowWorld/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HellowWorld.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        { 
+            string strRet = "The simplest case is to return a string from the controller.";
+            strRet += "This string will be drawn as text in the browser";
+            return strRet;
+
+        }
+
+        public string Welcome2()
+        {
+            string name = Request.Query["name"];
+            string num  = Request.Query["numtimes"];
+            return "Hello " + name + " num = " + num;
+        }
+
+    }
+}
+
+
+
+
+
+//public IActionResult Index()
+//{
+//    return View();
+//}
